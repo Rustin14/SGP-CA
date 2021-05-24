9606f689a1af4cff3001aea2ed0c10966ed0bdfe
REV09 <hector.morelos.607@gmail.com>
2021-05-24 14:18:15 -0500

    GUI anteproyecto terminado

---
+++ src/SGP/CA/GUI/AddObjectiveController.java
@@ -0,0 +1,22 @@
+package SGP.CA.GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AddObjectiveController extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML/AddObjectiveFXML.fxml"));
+        primaryStage.setTitle("añadir objetivo ");
+        primaryStage.setScene(new Scene(root, 900, 600));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
