a2bd4c7263299b7fee99007795aee0f796683f95
REV09 <hector.morelos.607@gmail.com>
2021-06-12 19:28:36 -0500

    GUI consultar proyecto de investigacion 100% funcional, falta blindar

--- src/SGP/CA/GUI/ConsultBluePrintController.java
+++ src/SGP/CA/GUI/ConsultBluePrintController.java
@@ -1,5 +1,6 @@
 package SGP.CA.GUI;
 
+import SGP.CA.Domain.BluePrint;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,8 +15,12 @@ import SGP.CA.DataAccess.BluePrintDAO;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class ConsultBluePrintController extends Application{
+
+    InvestigationProjectConsultController investigationProjectConsultController;
 
     @FXML
     private Button deleteButton;
@@ -88,6 +93,31 @@ public class ConsultBluePrintController extends Application{
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.initOwner(modifyButton.getScene().getWindow());
         stage.showAndWait();
+    }
+
+    public void getBluePrintTitle(InvestigationProjectConsultController stage2Controller, String bluePrintTitle) throws SQLException, ClassNotFoundException{
+        bluePrintTitleTextField.setText(bluePrintTitle);
+        investigationProjectConsultController = stage2Controller;
+        searchProject();
+    }
+
+
+    public void searchProject() throws SQLException, ClassNotFoundException{
+        BluePrintDAO bluePrintDAO = new BluePrintDAO();
+        BluePrint bluePrint = bluePrintDAO.searchBluePrintByTitle(bluePrintTitleTextField.getText());
+        DateFormat setDate = new SimpleDateFormat("dd/MM/yyyy");
+        String startDate = setDate.format(bluePrint.getStartDate());
+        startDateField.setText(startDate);
+        associatedLGAC.setText(bluePrint.getAssociatedLgac());
+        stateTextField.setText(bluePrint.getState());
+        directorTextField.setText(bluePrint.getDirector());
+        coDirectorTextField.setText(bluePrint.getCoDirector());
+        durationTextField.setText(String.valueOf(bluePrint.getDuration()));
+        modalityTextField.setText(bluePrint.getModality());
+        receptionWorkNameTextField.setText(bluePrint.getReceptionWorkName());
+        requirementsTextField.setText(bluePrint.getRequirements());
+        studentTextField.setText(bluePrint.getStudent());
+        descriptionTextArea.setText(bluePrint.getDescription());
     }
 
     public static void main(String[] args) {
--- src/SGP/CA/GUI/InvestigationProjectConsultController.java
+++ src/SGP/CA/GUI/InvestigationProjectConsultController.java
@@ -1,7 +1,7 @@
 package SGP.CA.GUI;
 
+import SGP.CA.Domain.BluePrint;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -12,6 +12,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import SGP.CA.Domain.InvestigationProject;
@@ -21,6 +22,7 @@ import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import SGP.CA.DataAccess.BluePrintDAO;
 
 public class InvestigationProjectConsultController extends Application{
 
@@ -57,9 +59,23 @@ public class InvestigationProjectConsultController extends Application{
     @FXML
     private Button addProjectButton;
 
+    @FXML
+    private ComboBox<String> bluePrintsComboBox;
+
+    @FXML
+    private TextField bluePrintTitleTextField;
+
+    @FXML
+    private TextField stateTextField;
+
+    @FXML
+    private Button consultButton;
+
     private ObservableList<String> investigationProjectTitles = FXCollections.observableArrayList();
 
     private ArrayList<InvestigationProject> investigationProjects = new ArrayList<>();
+
+    InvestigationProjectConsultController investigationProjectConsultController;
 
 
     @Override
@@ -121,14 +137,44 @@ public class InvestigationProjectConsultController extends Application{
         descriptionTextField.setText(investigationProject.getDescription());
     }
 
+    public void bluePrintsComboBoxEvent() throws SQLException, ClassNotFoundException{
+        String titleSelected = bluePrintsComboBox.getSelectionModel().getSelectedItem();
+        BluePrintDAO bluePrintDAO = new BluePrintDAO();
+        BluePrint bluePrint = bluePrintDAO.searchBluePrintByTitle(titleSelected);
+        stateTextField.setText(bluePrint.getState());
+        bluePrintTitleTextField.setText(bluePrint.getBluePrintTitle());
+    }
+
+    public void consultButtonEvent() throws IOException,SQLException,ClassNotFoundException{
+        Stage stage2 = new Stage();
+        FXMLLoader loader = new FXMLLoader();
+        AnchorPane root = (AnchorPane) loader.load(getClass().getResource("FXML/ConsultBluePrintFXML.fxml").openStream());
+        ConsultBluePrintController consultBluePrintController = (ConsultBluePrintController) loader.getController();
+        consultBluePrintController.getBluePrintTitle(investigationProjectConsultController,bluePrintTitleTextField.getText());
+        Scene scene = new Scene(root);
+        stage2.setScene(scene);
+        stage2.alwaysOnTopProperty();
+        stage2.initModality(Modality.APPLICATION_MODAL);
+        stage2.showAndWait();
+    }
+
     @FXML
     public void initialize() throws SQLException, ClassNotFoundException {
+        investigationProjectConsultController = this;
         InvestigationProjectDAO investigationProjectDAO = new InvestigationProjectDAO();
         investigationProjects = investigationProjectDAO.getAllInvestigationProjects();
         for(InvestigationProject investigationProject : investigationProjects){
             investigationProjectTitles.add(investigationProject.getProjectTitle());
         }
         comboBoxProjects.setItems(investigationProjectTitles);
+        BluePrintDAO bluePrintDAO = new BluePrintDAO();
+        ArrayList<BluePrint> bluePrints = new ArrayList<>();
+        bluePrints = bluePrintDAO.getAllBluePrints();
+        ObservableList<String> bluePrintTitles = FXCollections.observableArrayList();
+        for (BluePrint bluePrint: bluePrints){
+            bluePrintTitles.add(bluePrint.getBluePrintTitle());
+        }
+        bluePrintsComboBox.setItems(bluePrintTitles);
     }
 
     public static void main(String[] args) {
