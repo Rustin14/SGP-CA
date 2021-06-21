15b3ef0a3ea133d8ce59df3dc2d9374f9d44e4ed
REV09 <hector.morelos.607@gmail.com>
2021-06-21 13:44:45 -0500

    Alerta de confirmacion de modificacion de plan de trabajo funcional

---
+++ src/SGP/CA/GUI/ConfirmationModifyWorkPlanAlertController.java
@@ -0,0 +1,32 @@
+package SGP.CA.GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class ConfirmationModifyWorkPlanAlertController extends Application{
+
+    @FXML
+    private Button okButton;
+
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("FXML/ConfirmationModifyWorkPlanAlertFXML.fxml"));
+        primaryStage.setTitle("Confirmacion");
+        primaryStage.setScene(new Scene(root, 500, 200));
+        primaryStage.show();
+    }
+
+    public void okButtonEvent (){
+        Stage stage = (Stage) okButton.getScene().getWindow();
+        stage.close();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
